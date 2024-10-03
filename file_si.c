07d449e4b2c2144659091000e61ad8c4f0d4d5c1
Evans-Kipkorir-Rono <ronoevans57@gmail.com>
2024-10-03 16:28:35 +0300

    Update file simple interest

--- file_si.c
+++ file_si.c
@@ -1,4 +1,4 @@
-//program to print rules govern while studying in the library
+//program to calculate the simple interest 
 /*Author:Evans Kipkorir
 Date:24/09/2024
 Registration number:CT101/G/22634/24
